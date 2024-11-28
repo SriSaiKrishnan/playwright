56c1d92e5beab4ec357d5aaca2620e9f0b5aa334
SriSaiKrishnan <krishnansai99@gmail.com>
2024-11-28 11:33:55 +0530

    Test Generator Practice

---
+++ src/main/java/playwrightpractice/TestGenerator.java
@@ -0,0 +1,37 @@
+package playwrightpractice;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+public class TestGenerator {
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://toolsqa.com/");
+            Page page1 = page.waitForPopup(() -> {
+                page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Demo Site")).click();
+            });
+            page1.locator("div:nth-child(2) > div > .avatar > svg").click();
+            page1.getByText("Practice Form").click();
+            page1.getByPlaceholder("First Name").click();
+            page1.getByPlaceholder("First Name").fill("test");
+            page1.getByPlaceholder("First Name").press("Tab");
+            page1.getByPlaceholder("Last Name").fill("practice");
+            page1.getByPlaceholder("name@example.com").click();
+            page1.getByPlaceholder("name@example.com").fill("test@email");
+            page1.getByText("Male", new Page.GetByTextOptions().setExact(true)).click();
+            page1.getByPlaceholder("Mobile Number").click();
+            page1.getByPlaceholder("Mobile Number").fill("7843657843");
+            page1.locator("#dateOfBirthInput").click();
+            page1.getByLabel("Choose Thursday, November 28th,").click();
+            page1.locator(".subjects-auto-complete__value-container").click();
+            page1.locator("#subjectsInput").fill("test");
+            page1.getByText("Sports").click();
+        }
+    }
+
+}
